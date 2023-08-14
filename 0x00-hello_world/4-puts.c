3508099a9feab9a682fea93bb9ba6cc871f92144
Hope Hlungwane <hope24rifumo@gmain.com>
2023-08-14 03:59:09 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multiligual puzzle\n");
+	return (0);
+}
+		
