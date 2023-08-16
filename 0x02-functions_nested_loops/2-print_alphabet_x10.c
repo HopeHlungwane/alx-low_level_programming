f0dacbafa0cf49f724435f575ba05b1124ebac12
Hope Hlungwane <hope24rifumo@gmain.com>
2023-08-16 02:22:28 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,24 +3,26 @@
 /**
  * print_alphabet_x10 - prints alphabet 10 times
  *
- * Return:void
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
+
 	char x;
+
 	int i;
+
 	i = 0;
 
 	while (i < 10)
 	{
-		for (x = 'a' ; x <= 'z' ; x++)
+		for (x = 'a'; x <= 'z'; x++)
 		{
 			_putchar(x);
 		}
 		_putchar('\n');
 		i++;
 	}
-
 }
 
